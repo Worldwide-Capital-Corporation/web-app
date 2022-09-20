799a28a305be1ab91bb38fda9d3ea47c2280796d
Innocent Magagula <innocentm@globalkinetic.com>
2022-09-20 18:17:08 +0200

    fix(mifos): valid urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,8 +8,7 @@ export const environment = {
   // For connecting to server running elsewhere update the tenant identifier
   fineractPlatformTenantId: window['env']['fineractPlatformTenantId'] || 'default',
   // For connecting to others servers running elsewhere update the base API URL
-  baseApiUrls: window['env']['fineractApiUrls'] ||
-    'https://dev.mifos.io,https://demo.mifos.io,https://qa.mifos.io,https://staging.mifos.io,https://mobile.mifos.io,https://demo.fineract.dev,https://localhost:8443',
+  baseApiUrls: window['env']['fineractApiUrls'] || 'https://localhost:8443,https://137.184.51.196:8443',
   // For connecting to server running elsewhere set the base API URL
   baseApiUrl: window['env']['fineractApiUrl'] || 'https://137.184.51.196:8443',
   allowServerSwitch: env.allow_switching_backend_instance,
